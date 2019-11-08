0c23d5dc3711577474a588622f5a138cdcbc6ff5
Alex Lawry-White <designalexweb@gmail.com>
2019-11-08 18:40:32 +0000

    sdf

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Form extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      globalTitle: 'lame-duck title',
+      title: ''
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount () {
+    console.log('Rendered Form')
+  }
+
+  onInputChange = e => {
+    this.setState({ title: e.target.value })
+  }
+
+  onButtonClick = e => {
+    this.setState({ globalTitle: 'My form -' })
+    e.preventDefault()
+  }
+
+  componentDidUpdate () {
+    console.log('Title changed')
+  }
+
+  render () {
+    return (
+      <div>
+        <form>
+          <h1>{this.state.globalTitle}</h1>
+          <input onChange={this.onInputChange} value={this.state.title} />
+          <button onClick={this.onButtonClick}>Validate</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Form />, document.querySelector('#root'))
